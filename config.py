743bb14078600616eb911e55f7cccb4bcf5862ca
IvanSlivenko <ivan8822@ukr.net>
2024-06-06 11:24:34 +0300

    06-06-2024-11-24

--- config.py
+++ config.py
@@ -2,3 +2,11 @@ TOKEN='7023058907:AAENFKpa-Zf1wFNS6hIA5rHsTAA8nKMHUdc'
 
 WEATHER_KEY='7d3ff9b16b029632188557e58d1f4fb8'
 
+
+url_judo='https://www.google.com/search?client=opera&q=lp.lj+evfym&sourceid=opera&ie=UTF-8&oe=UTF-8#lpg=cid:CgIgAQ%3D%3D,ik:CAoSLEFGMVFpcFBEZklZWmFHQTJUQk5rRmNRRWdvSlBiRGxrT2ZWM1hyWC15SFpa'
+url_Google='https://www.google.com/?hl=ru'
+
+url_Bild_Expr='https://bud-express.in.ua'
+url_Bild_Expr_doors='https://bud-express.in.ua/categories/d38241db-946c-4fbc-8338-eba2e95ade7e'
+url_Bild_Expr_heating='https://bud-express.in.ua/categories/43dbb77f-d0a2-46f2-bd47-fb5786073407'
+
---
+++ main_8.py
@@ -0,0 +1,64 @@
+
+
+import asyncio
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.types import Message, CallbackQuery, FSInputFile, InlineKeyboardButton, InlineKeyboardMarkup, callback_query, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.filters import CommandStart, Command, or_f
+from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
+from aiogram.types.web_app_info import WebAppInfo
+
+#------------------------------------------------------- initial bot
+from config import TOKEN
+from config import url_Bild_Expr, url_Bild_Expr_doors
+#---------------------------------------- TOKEN
+bot = Bot(TOKEN)
+dp = Dispatcher()
+#-------------------------------------------------------
+
+
+
+@dp.message(CommandStart())
+async def cmd_start(message: types.Message):
+    markup = ReplyKeyboardBuilder()
+    markup.add(KeyboardButton(text='Google', web_app=WebAppInfo(url='https://www.google.com.ua/?hl=uk')))
+    markup.add(KeyboardButton(text='BudExpress', web_app=WebAppInfo(url=str(url_Bild_Expr))))
+    markup.add(KeyboardButton(text='BudExpress Двері', web_app=WebAppInfo(url=str(url_Bild_Expr_doors))))
+    await message.answer(f'{message.from_user.first_name}. Вітаємо !', reply_markup=markup.adjust(1,2).as_markup(resize_keyboard=True))
+    
+
+
+
+
+#-----------------------------------------------------------------------Відправляємо фото та текст у відповідь
+@dp.message(F.text == 'test')
+async def test(message: types.Message):
+    await message.reply('the test is started')
+    file = FSInputFile('./images/Screenshot_1.jpg', 'rb')
+    await message.answer_photo(photo=file)   
+
+ 
+    
+
+
+
+    
+   
+
+
+
+
+   
+
+
+#----------------------------------------- run
+async def main():
+    
+    
+    await dp.start_polling(bot)
+
+if __name__  == '__main__':
+    print('Бот розпочав роботу')
+    try:
+        asyncio.run(main())  
+    except KeyboardInterrupt:
+        print('Бот вимкнуто')    
