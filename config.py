97caabcff7a2ed782a3305575abd65be396e94d8
IvanSlivenko <ivan8822@ukr.net>
2024-06-07 21:54:43 +0300

    07-06-2024-21-54

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 TOKEN='7023058907:AAENFKpa-Zf1wFNS6hIA5rHsTAA8nKMHUdc'
+PAYMENT_TOKEN='1877036958:TEST:0079a5a0d5b0c8c7b45d47443cbc65b67422a907'
 
 WEATHER_KEY='7d3ff9b16b029632188557e58d1f4fb8'
 
@@ -9,5 +10,8 @@ url_Google='https://www.google.com/?hl=ru'
 url_Bild_Expr='https://bud-express.in.ua'
 url_Bild_Expr_doors='https://bud-express.in.ua/categories/d38241db-946c-4fbc-8338-eba2e95ade7e'
 url_Bild_Expr_heating='https://bud-express.in.ua/categories/43dbb77f-d0a2-46f2-bd47-fb5786073407'
-url_custome_1='https://github.com/IvanSlivenko/my_host/blob/main/index.html'
+
+url_custome_1='https://umanproger.000webhostapp.com/?'
+
+# url_custome_2='https://files.000webhost.com/index.html'
 
